5af6bf21d86e1c1dc21a9a56c07aa1465a2bf396
Jaeyoung <jalpr16@gmail.com>
2022-01-20 15:48:24 +0900

    updated template.hs

--- template.hs
+++ template.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 import Control.Applicative (liftA2)
 import Control.Arrow ((>>>))
@@ -8,8 +9,12 @@ import Control.Monad.State (State, evalState, get, gets, put)
 import qualified Data.ByteString.Lazy.Char8 as C
 import Data.Function
 import Data.Int
-import Data.List
+import Data.List (groupBy)
+import Data.List.NonEmpty (NonEmpty, head, tail)
+import qualified Data.List.NonEmpty as NE
 import Data.Maybe
+import Prelude hiding (fromJust, head, tail)
+import qualified Prelude
 
 main :: IO ()
 main = C.interact $ runScanner input >>> solve >>> output
@@ -29,7 +34,9 @@ output = undefined
 solve :: Input -> Output
 solve = undefined
 
------------------------------------ Template -----------------------------------
+--------------------------------------------------------------------------------
+-- Template: convenience utilities.
+--------------------------------------------------------------------------------
 groupOn :: Eq b => (a -> b) -> [a] -> [[a]]
 groupOn = groupBy . on (==)
 
@@ -37,11 +44,11 @@ chunksOf :: Int -> [a] -> [[a]]
 chunksOf _ [] = []
 chunksOf k xs = let (hs, ts) = splitAt k xs in hs : chunksOf k ts
 
-adjacents :: [a] -> [(a, a)]
-adjacents xs = zip xs (tail xs)
+adjacents :: NonEmpty a -> [(a, a)]
+adjacents = adjacentsWith (,)
 
-adjacentsWith :: (a -> a -> b) -> [a] -> [b]
-adjacentsWith f xs = zipWith f xs (tail xs)
+adjacentsWith :: (a -> a -> b) -> NonEmpty a -> [b]
+adjacentsWith f xs = zipWith f (NE.toList xs) (tail xs)
 
 count :: Eq a => a -> [a] -> Int
 count = countBy . (==)
@@ -49,6 +56,9 @@ count = countBy . (==)
 countBy :: (a -> Bool) -> [a] -> Int
 countBy p = length . filter p
 
+--------------------------------------------------------------------------------
+-- Template: Scanner functions. Most of these are deliberately unsafe.
+--------------------------------------------------------------------------------
 type Scanner = State [C.ByteString]
 
 runScanner :: Scanner a -> C.ByteString -> a
@@ -58,7 +68,7 @@ runScannerWith :: (C.ByteString -> [C.ByteString]) -> Scanner a -> C.ByteString 
 runScannerWith t s = evalState s . t
 
 peek :: Scanner C.ByteString
-peek = gets head
+peek = gets Prelude.head
 
 bstr :: Scanner C.ByteString
 bstr = get >>= \case [] -> return ""; s : ss -> put ss >> return s
@@ -88,7 +98,7 @@ numberOf :: Scanner a -> Scanner [a]
 numberOf s = int >>= flip replicateM s
 
 many :: Scanner a -> Scanner [a]
-many s = get >>= \case [] -> return []; _ -> (:) <$> s <*> many s
+many s = get >>= \case [] -> return []; _ -> liftA2 (:) s (many s)
 
 till :: (C.ByteString -> Bool) -> Scanner a -> Scanner [a]
 till p s = do
