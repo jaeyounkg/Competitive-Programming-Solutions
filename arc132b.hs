a8d0b92587900186d8bfcce81ed1635e89c4ce1b
Jaeyoung <jalpr16@gmail.com>
2022-01-20 20:45:05 +0900

    update

--- arc132b.hs
+++ arc132b.hs
@@ -6,15 +6,14 @@ import Control.Applicative (liftA2)
 import Control.Arrow ((>>>))
 import Control.Monad
 import Control.Monad.State (State, evalState, get, gets, put)
-import qualified Data.ByteString.Char8 as C
-import Data.Char
+import qualified Data.ByteString.Lazy.Char8 as C
 import Data.Function
 import Data.Int
-import Data.List hiding (head, tail)
+import Data.List (groupBy)
 import Data.List.NonEmpty (NonEmpty, head, tail)
 import qualified Data.List.NonEmpty as NE
 import Data.Maybe
-import Prelude hiding (head, tail)
+import Prelude hiding (fromJust, head, tail)
 import qualified Prelude
 
 main :: IO ()
@@ -22,41 +21,22 @@ main = C.interact $ runScanner input >>> solve >>> output
 
 -- main = C.interact $ runScanner (numberOf input) >>> map (solve >>> output) >>> C.unlines
 
-type Input = ()
+type Input = [Int]
 
-type Output = ()
+type Output = Int
 
 input :: Scanner Input
-input = undefined
+input = replicateM 3 int
 
 output :: Output -> C.ByteString
-output = undefined
+output = showB
 
 solve :: Input -> Output
-solve = undefined
+solve [n, p1, p2] = if p1 == 1 && p2 == 2 then 0 else min (n - p1 + 1) (p1 + 1)
 
 --------------------------------------------------------------------------------
 -- Template: convenience utilities
 --------------------------------------------------------------------------------
-groupOn :: Eq b => (a -> b) -> [a] -> [[a]]
-groupOn = groupBy . on (==)
-
-chunksOf :: Int -> [a] -> [[a]]
-chunksOf _ [] = []
-chunksOf k xs = let (hs, ts) = splitAt k xs in hs : chunksOf k ts
-
-adjacents :: NonEmpty a -> [(a, a)]
-adjacents = adjacentsWith (,)
-
-adjacentsWith :: (a -> a -> b) -> NonEmpty a -> [b]
-adjacentsWith f xs = zipWith f (NE.toList xs) (tail xs)
-
-count :: Eq a => a -> [a] -> Int
-count = countBy . (==)
-
-countBy :: (a -> Bool) -> [a] -> Int
-countBy p = length . filter p
-
 showB :: Show a => a -> C.ByteString
 showB = C.pack . show
 
@@ -71,48 +51,8 @@ runScanner = runScannerWith C.words
 runScannerWith :: (C.ByteString -> [C.ByteString]) -> Scanner a -> C.ByteString -> a
 runScannerWith t s = evalState s . t
 
-peek :: Scanner C.ByteString
-peek = gets Prelude.head
-
 bstr :: Scanner C.ByteString
 bstr = get >>= \case [] -> return ""; s : ss -> put ss >> return s
 
-str :: Scanner String
-str = C.unpack <$> bstr
-
-readStr :: Read a => Scanner a
-readStr = read <$> str
-
 int :: Scanner Int
 int = fst . fromJust . C.readInt <$> bstr
-
-integer :: Scanner Integer
-integer = readStr
-
-int64 :: Scanner Int64
-int64 = readStr
-
-double :: Scanner Double
-double = readStr
-
-decimal :: Int -> Scanner Int
-decimal p = round . ((10 ^ p) *) <$> double
-
-numberOf :: Scanner a -> Scanner [a]
-numberOf s = int >>= flip replicateM s
-
-many :: Scanner a -> Scanner [a]
-many s = get >>= \case [] -> return []; _ -> liftA2 (:) s (many s)
-
-till :: (C.ByteString -> Bool) -> Scanner a -> Scanner [a]
-till p s = do
-  t <- peek
-  if p t
-    then return []
-    else (:) <$> s <*> till p s
-
-two, three, four :: Scanner a -> Scanner [a]
-[two, three, four] = map replicateM [2 .. 4]
-
-pair :: Scanner a -> Scanner b -> Scanner (a, b)
-pair = liftA2 (,)
