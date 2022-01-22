8ebfa3eacd6432f51b42c58b9c0f7dfabc816925
Jaeyoung <jalpr16@gmail.com>
2022-01-23 01:27:18 +0900

    solved arc132c

--- arc132c.hs
+++ arc132c.hs
@@ -7,33 +7,89 @@ import Control.Applicative
 import Control.Arrow ((>>>))
 import Control.Monad
 import Control.Monad.State (State, evalState, get, gets, put)
-import Data.Array.IArray (Array, (!))
+import Data.Array.IArray (Array, (!), (//))
 import qualified Data.Array.IArray as A
 import Data.Bits
 import qualified Data.ByteString.Char8 as C
 import Data.Char
 import Data.Function
 import Data.Int
-import Data.List (filter, groupBy, length, splitAt, zipWith)
+import Data.List hiding (head, tail)
 import Data.List.NonEmpty (NonEmpty, head, tail)
 import qualified Data.List.NonEmpty as NE
 import Data.Maybe
+import Debug.Trace (traceShow, traceShowId)
 import Prelude hiding (head, tail)
 import qualified Prelude
 
 main :: IO ()
 main = C.interact $ runScanner input >>> solve >>> output
 
-type Input = ()
+type Input = (Int, Int, Array Int Int)
 
-type Output = ()
+type Output = Int
 
-input = undefined
+input = do
+  (n, d) <- two int
+  as <- lotsOf int
+  pure (n, d, A.listArray (1, n) as)
 
-output = undefined
+output = showB
+
+plus a b = (a + b) `mod` 998244353
 
 solve :: Input -> Output
-solve = undefined
+solve (n, d, as) = foldl1' plus $ A.elems $ foldl' update dp_init [1 .. n]
+  where
+    bnds = (0, 1 `shiftL` (d * 2 + 1) - 1)
+
+    -- states
+    -- defined as a lazy Array for memoization
+    s_base :: Array Int Int
+    s_base =
+      A.listArray
+        (0, n)
+        [ let f = \k -> fromEnum (A.inRange (1, n) (i + k) && as ! (i + k) > 0) `shiftL` (d + k)
+           in sum $ map f [- d .. d]
+          | i <- [0 .. n]
+        ]
+    s_init = s_base ! 0
+
+    -- dp[i][s]: i番目までおいてsの状態（固定されたiを含む）になったときの組み合わせの数
+    dp_init :: Array Int Int
+    dp_init = A.listArray bnds (repeat 0) // [(s_init, 1)]
+
+    update dp_prev i = A.listArray bnds $ map compute (A.range bnds)
+      where
+        prev = flip shiftL 1 . flip clearBit (d * 2)
+        -- edge cases
+        compute s =
+          if
+              -- including unavailable bits
+              | any (testBit s . (+ d)) . filter (\k -> not $ A.inRange (1, n) (i + k)) $ [- d .. d] -> 0
+              -- not including s_base
+              | (s `xor` s_base ! i) .&. s_base ! i > 0 -> 0
+              -- already put, or putting at i + d
+              | i `elem` as || (s `xor` s_base ! i) `testBit` (d * 2) -> (dp_prev ! prev s) `plus` (dp_prev ! (prev s + 1))
+              | otherwise -> compute' s
+        -- cons cases
+        compute' s =
+          let s_added = s `xor` s_base ! i
+              p k = testBit s_added (d + k)
+              f k =
+                let prev_s = prev $ clearBit s (d + k)
+                 in (dp_prev ! prev_s) `plus` (dp_prev ! (prev_s + 1))
+           in (sum . map f . filter p) [- d .. d - 1]
+
+-- Debugging
+
+traceShowDp dp = traceShow (filter (\(_, v) -> v > 0) . A.assocs $ dp)
+
+solveDumber (n, d) = filter p $ permutations [1 .. n]
+  where
+    p perm = all (\(i, v) -> abs (i - v) <= d) (A.assocs arr)
+      where
+        arr = A.listArray (1, n) perm :: Array Int Int
 
 --------------------------------------------------------------------------------
 -- Template: convenience utilities
